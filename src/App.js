7e9140579dd24ec90288e6d38c9e95506352b3e8
Hasan Koçak <hsn.kocak@hotmail.com>
2025-09-18 19:33:43 +0300

    Güncellenmiş dosyaları ekledim

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
-
-// Bileşenler
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ScrollToTopButton from './components/ScrollToTop'; // ScrollToTop'u yeniden isimlendirdik
-import ScrollToTopOnNavigate from './components/ScrollToTopOnNavigate';
-
-// Sayfalar
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ServicesPage from './pages/ServicesPage';
-import ContactPage from './pages/ContactPage';
-import TeamPage from './pages/TeamPage'; // Yeni Ekip Sayfası
 
 function App() {
   return (
-    <BrowserRouter>
-      <ScrollToTopOnNavigate />
-      <Header />
-      <main>
-        {/* Sayfa geçiş animasyonları için bir sarmalayıcı eklenebilir */}
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/hakkimizda" element={<AboutPage />} />
-          <Route path="/hizmetlerimiz" element={<ServicesPage />} />
-          <Route path="/ekibimiz" element={<TeamPage />} />
-          <Route path="/iletisim" element={<ContactPage />} />
-        </Routes>
-      </main>
-      <Footer />
-      <ScrollToTopButton />
-    </BrowserRouter>
+    <div style={{ padding: '50px', color: 'white', fontSize: '24px' }}>
+      <h1>Test Başarılı</h1>
+      <p>Bu sayfa göründüyse, temel React uygulaması çalışıyor demektir.</p>
+    </div>
   );
 }
 
